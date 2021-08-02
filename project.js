0c2a06ba4c8341646e22f11866fa9c9d3d67ddad
Katherine Gardner <katiegardner@Katherines-MacBook-Air.local>
2021-08-02 14:24:51 -0700

    added test

--- project.js
+++ project.js
@@ -2,4 +2,9 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
-module.exports = getGreeting;
+// module.exports = getGreeting;
+module.exports = {
+    getGreeting,
+    add: function(num1, num2) { return num1 + num2 }
+
+}
--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-const getGreeting = require('./project');
+const {getGreeting, add} = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
+});
+
+test('add these bananas', () => {
+  expect(add (1, 2)).toEqual(3);
 });
